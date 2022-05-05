8b600e733fe64ed704915f237f079bc538bd4840
Donald Maisiba <61703882+Donald471-prog@users.noreply.github.com>
2022-05-05 22:18:06 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://39xk0wspo3.execute-api.eu-west-1.amazonaws.com/default/My_lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
